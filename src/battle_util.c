63395982d2bd4fb7dd7e63c476dff4910864385b
Demonheadge <64343182+Demonheadge@users.noreply.github.com>
2024-08-01 00:39:23 +0800

    mega evolution fix

--- src/battle_util.c
+++ src/battle_util.c
@@ -10295,7 +10295,7 @@ bool32 CanMegaEvolve(u32 battler)
 
     // Check if Player has a Mega Ring
     if ((GetBattlerPosition(battler) == B_POSITION_PLAYER_LEFT || (!(gBattleTypeFlags & BATTLE_TYPE_MULTI) && GetBattlerPosition(battler) == B_POSITION_PLAYER_RIGHT))
-     && !CheckBagHasItem(ITEM_MEGA_RING, 1))
+     && !CheckBagHasItem(ITEM_FRAGMENT_OF_JAS, 1))
         return FALSE;
 
     // Check if trainer already mega evolved a pokemon.
