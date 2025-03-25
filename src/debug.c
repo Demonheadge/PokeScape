0b1285b4c8141ff7f812514a5b03ab3d6b7078ce
Demonheadge <64343182+Demonheadge@users.noreply.github.com>
2025-03-25 16:15:03 +0800

    river lum thicc no more.

--- src/debug.c
+++ src/debug.c
@@ -464,8 +464,8 @@ static const u8 sDebugText_Util_Script_2[] =               _("SLAYER VAR CHECK")
 static const u8 sDebugText_Util_Script_3[] =               _("LEVEL CAP");
 static const u8 sDebugText_Util_Script_4[] =               _("TOKKUL");
 static const u8 sDebugText_Util_Script_5[] =               _("TOGGLE BATTLE SPEED");
-static const u8 sDebugText_Util_Script_6[] =               _("Script 6");
-static const u8 sDebugText_Util_Script_7[] =               _("Script 7");
+static const u8 sDebugText_Util_Script_6[] =               _("FRIENDSHIP CHECKER");
+static const u8 sDebugText_Util_Script_7[] =               _("GNOMEBOY KEVIN");
 static const u8 sDebugText_Util_Script_8[] =               _("Script 8");
 // Util Menu
 static const u8 sDebugText_Util_FlyToMap[] =                 _("Fly to map…{CLEAR_TO 110}{RIGHT_ARROW}");
