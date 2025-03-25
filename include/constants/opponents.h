f38b74ff67dc0dcac869b93b54e74142c3989e56
A-Twisted-Smile <158811861+A-Twisted-Smile@users.noreply.github.com>
2025-03-25 14:53:04 +0000

    Miscellaneous Tweaks
    - Adding in further rebalancing changes to NPCs following Stan's playtest
    - Rebalancing some wild fishing and surf encounter levels
    - Adding in dialogue for new NPCs that have popped up around Lumbridge following the rework to the river
    - Added in some additional NPCs with dialogue to give players more insight into monster evolutions and types

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -523,13 +523,15 @@
 #define TRAINER_ROUTE26_BLACK_KNIGHT                    517
 #define TRAINER_ICE_MOUNTAIN_GUARD_1                    518
 #define TRAINER_ICE_MOUNTAIN_GUARD_2                    519
+#define TRAINER_ROUTE21_TRAINER_3                       520
+#define TRAINER_ROUTE22_TRAINER_2                       521
 
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      (TRAINER_ICE_MOUNTAIN_GUARD_2 + 1)
+#define TRAINERS_COUNT                      (TRAINER_ROUTE22_TRAINER_2 + 1)
 #define MAX_TRAINERS_COUNT                  864
 
 #endif  // GUARD_CONSTANTS_OPPONENTS_H
